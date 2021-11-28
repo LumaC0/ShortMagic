3dfe904ad123a16aa8c687aa3187e6989f7f2461
Spencer Finkel <shf16@my.fsu.edu>
2021-11-27 23:40:35 -0600

    Added dynamic paths and shortcut file creation

--- shortmagic.py
+++ shortmagic.py
@@ -1,37 +1,29 @@
-# This code can be put in any Python module, it does not require IPython
-# itself to be running already.  It only creates the magics subclass but
-# doesn't instantiate it yet.
 from __future__ import print_function
 from os import getenv
-from pathlib import Path as path
+from pathlib import Path as pl
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic)
+from IPython import paths as ip
 from dotenv import load_dotenv
 
-load_dotenv()
-
-DIR = path().absolute().join(getenv("SHORTCUT_DIR"))
-#DIR = "/Users/spencerfinkel/.ipython/extensions/custom_magics"
+DIR = pl(f"{ip.get_ipython_package_dir()}/extensions/shortcutmagic")
+load_dotenv(f"{DIR}/.env")
 
 # Custom ipython magic method documentation at: 
 # https://ipython.readthedocs.io/en/stable/config/custommagics.html
-# The class MUST call this class decorator at creation time
 
 @magics_class
 class ShortMagic(Magics):
 
-
     def __init__(self, shell):
         super(ShortMagic, self).__init__(shell)
-
-        self.vim_file = "vshortcuts.txt"
-        self.ip_file  = "ishortcuts.txt"
-
-        self._create_file()
+        self.apps = getenv("APPLICATIONS").split()
+        self.scdir = DIR.joinpath(getenv("SHORTCUT_DIR_NAME"))
+        self._create_files()
 
 
     @line_magic
-    def isc(self, line):
+    def sc(self, line):
         "Get a user defined list of IPython shortcuts"
         file = self.ip_file
         coms = self._open_for_read(file)
@@ -41,23 +33,23 @@ class ShortMagic(Magics):
 
 
     @line_magic
-    def isc_add(self, line):
+    def sc_add(self, line):
         "Add to IPython shortcuts file - ishortcuts.txt"
         file = self.ip_file
         self._open_for_append(file)
 
 
     @line_magic
-    def isc_edit(self,line):
+    def sc_edit(self,line):
         file = self.ip_file
         self._edit_file(file)
 
 
-    def _create_file(self):
-        for file in (self.vim_file, self.ip_file):
-            path = f"{DIR}/{file}"
-            if not Path(path).exists():
-                Path(path).touch()
+    def _create_files(self):
+        for app in self.apps:
+            appath = self.scdir.joinpath(f"{app.strip()}.txt")
+            if not appath.exists():
+                appath.touch()
 
 
     def _write_command(self):
@@ -84,7 +76,6 @@ class ShortMagic(Magics):
 
     def _edit_file(self, file=None):
         coms = self._open_for_read(file)
-
         com_dict = {}
         for i in coms:
             k, v = i.split(SEP)
